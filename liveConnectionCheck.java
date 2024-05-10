3751b1d34c9b6477b3d7544337fdf6ae6066d632
Omer Basar <78821009+omerfarukbasar@users.noreply.github.com>
2024-05-10 17:27:28 -0400

    Test disconnect procedure

---
+++ liveConnectionCheck.java
@@ -0,0 +1,43 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class liveConnectionCheck {
+    // Port to be used for connection check
+    private static final int PORT = 8988;
+
+    // Protocol for connecting server, initiated by hitting connect
+    private void clientStatus(String opponentIP) {
+        try (Socket socket = new Socket(opponentIP, PORT);){
+            System.out.println("Started 'connection check' socket on Port " + PORT  +"\n");
+            // Setup data streams
+            DataInputStream fromServer = new DataInputStream(socket.getInputStream());
+            DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
+
+            while (true)
+                continue;
+        }
+        catch (IOException e) {System.err.println("Connection error: " + e.getMessage());}
+        catch (Exception e) {System.err.println("Other Connection error: " + e.getMessage());}
+    }
+
+    // Protocol for starting up the server
+    private void hostStatus() {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Started 'connection check' socket on Port " + PORT  +"\n");
+
+            Socket socket = serverSocket.accept();
+
+            // Setup streams
+            DataInputStream fromClient = new DataInputStream(socket.getInputStream());
+            DataOutputStream toClient = new DataOutputStream(socket.getOutputStream());
+
+            while (true)
+                continue;
+        }
+        catch (IOException e) {System.err.println("Server error: " + e.getMessage());}
+        catch (Exception e) {System.out.println("Other server error: " + e.getMessage());}
+    }
+}
